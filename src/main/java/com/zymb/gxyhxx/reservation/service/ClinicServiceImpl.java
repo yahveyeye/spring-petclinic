611fe534e7ecc0783e62ba56645a3abc68f49d60
linyuan <linyuan@live.cn>
2015-11-07 19:33:22 +0800

    make the method transactional

--- src/main/java/com/zymb/gxyhxx/reservation/service/ClinicServiceImpl.java
+++ src/main/java/com/zymb/gxyhxx/reservation/service/ClinicServiceImpl.java
@@ -28,62 +28,58 @@ import com.zymb.gxyhxx.reservation.repository.OwnerRepository;
 import com.zymb.gxyhxx.reservation.repository.ReservationRepository;
 
 /**
- * Mostly used as a facade for all Petclinic controllers
- * Also a placeholder for @Transactional and @Cacheable annotations
+ * Mostly used as a facade for all Petclinic controllers Also a placeholder
+ * for @Transactional and @Cacheable annotations
  *
  * @author Michael Isvy
  */
 @Service
 public class ClinicServiceImpl implements ClinicService {
 
-  
-    private OwnerRepository ownerRepository;
-    private ReservationRepository reservationRepository;
+	private OwnerRepository ownerRepository;
+	private ReservationRepository reservationRepository;
 
-    @Autowired
-    public ClinicServiceImpl(OwnerRepository ownerRepository, ReservationRepository reservationRepository) {
-    	super();
-    	this.ownerRepository = ownerRepository;
-    	this.reservationRepository = reservationRepository;
-    }
-  
-
-    @Override
-    @Transactional(readOnly = true)
-    public Owner findOwnerById(int id) throws DataAccessException {
-        return ownerRepository.findById(id);
-    }
-
+	@Autowired
+	public ClinicServiceImpl(OwnerRepository ownerRepository, ReservationRepository reservationRepository) {
+		super();
+		this.ownerRepository = ownerRepository;
+		this.reservationRepository = reservationRepository;
+	}
 
 	@Override
-    @Transactional(readOnly = true)
-    public Collection<Owner> findOwnerByLastName(String lastName) throws DataAccessException {
-        return ownerRepository.findByLastName(lastName);
-    }
-
-    @Override
-    @Transactional
-    public void saveOwner(Owner owner) throws DataAccessException {
-        ownerRepository.save(owner);
-    }
-
-
- 
+	@Transactional(readOnly = true)
+	public Owner findOwnerById(int id) throws DataAccessException {
+		return ownerRepository.findById(id);
+	}
 
 	@Override
+	@Transactional(readOnly = true)
+	public Collection<Owner> findOwnerByLastName(String lastName) throws DataAccessException {
+		return ownerRepository.findByLastName(lastName);
+	}
+
+	@Override
+	@Transactional
+	public void saveOwner(Owner owner) throws DataAccessException {
+		ownerRepository.save(owner);
+	}
+
+	@Override
+	@Transactional
 	public void saveReservation(Reservation reservation) {
 		reservationRepository.save(reservation);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Collection<Reservation> findReservationByLastName(String lastName) {
-		return	reservationRepository.findByLastName(lastName);
+		return reservationRepository.findByLastName(lastName);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Reservation findReservationById(int reservationId) {
 		return reservationRepository.findById(reservationId);
 	}
-
 
 }
